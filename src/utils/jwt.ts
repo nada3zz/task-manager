df5dca7f261698d7fd47e15e4e6726425bb584c5
Nada EzzEldin <Nada_Ezzdin@AtheelCC.local>
2025-12-03 03:00:54 +0200

    add expire time

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -4,6 +4,7 @@ import { ACCESS_TOKEN_SECRET as accessTokenSecret } from "../config";
 
 export const signJwt = (payload: object, options?: jwt.SignOptions): string => {
   return jwt.sign(payload, accessTokenSecret, {
+    expiresIn: "1h",
     ...options,
   });
 };
